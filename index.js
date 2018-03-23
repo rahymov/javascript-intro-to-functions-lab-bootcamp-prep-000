9e9577bb6830254e031bd75069824c38a8a9607c
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-03-23 12:46:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string){
-  if(string === string.toLowerCase){
+  if(string === string.toLowerCase()){
     return "I can\'t hear you!";
   }
   else if(string ===string.toUpperCase()){
