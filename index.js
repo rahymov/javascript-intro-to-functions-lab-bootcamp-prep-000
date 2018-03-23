d48fe48a83ead03a6e2e916ec42a50822f838388
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-03-23 12:41:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 function shout(string){
-  return string.toUpperCase()
-  var uppercase = "HELLO!"
-  uppercase.toUpperCase() === uppercase
   
-  var lowercase = "hello!"
-  lowercase.toLowerCase() === lowercase
+  var uppercase = "HELLO!";
+  uppercase.toUpperCase() === uppercase;
   
-  var mixedCase = 'Hi there!'
+  var lowercase = "hello!";
+  lowercase.toLowerCase() === lowercase;
   
-  mixedCase.toLowerCase() === mixedCase
-  mixedCase.toUpperCase() === mixedCase
+  var mixedCase = 'Hi there!';
+  
+  mixedCase.toLowerCase() === mixedCase;
+  mixedCase.toUpperCase() === mixedCase;
+  return string.toUpperCase();
+}
+function whisper(string){
+  var lowercase = "hello";
+  lowercase.toLowerCase() === lowercase;
+  var uppercase = "HELLO";
+  uppercase.toLowerCase() === lowercase;
+  
+  var mixedCase = 'Hi there!';
+  
+  mixedCase.toLowerCase() === mixedCase;
+  mixedCase.toLowerCase() === mixedCase;
+  return string.toLowerCase();
+}
+function logShout(string){
+  console.log(string.toUpperCase());
+}
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+function sayHiToGrandma(string){
+  if(string === string.toLowerCase){
+    return "I can\'t hear you!";
+  }
+  else{
+    var 
+    return
+  }
 }
